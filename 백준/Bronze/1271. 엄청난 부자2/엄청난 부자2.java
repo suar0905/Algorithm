e51746b6157841f2892df810024b54eea7f9ac58
HyunSoo Oh <87848396+suar0905@users.noreply.github.com>
2025-04-27 17:38:39 +0900

    [Bronze V] Title: 엄청난 부자2, Time: 184 ms, Memory: 17848 KB -BaekjoonHub

---
+++ 백준/Bronze/1271. 엄청난 부자2/엄청난 부자2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.math.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        BigInteger a = sc.nextBigInteger();
+        BigInteger b = sc.nextBigInteger();
+        
+        System.out.println(a.divide(b));
+        System.out.println(a.remainder(b));
+    }
+}
